a63586d3f4301635a4f1669dc42e910724ea2190
Shahadat Hossain <shahadat84sh@gmail.com>
2023-12-05 12:30:40 +0600

    Need soe backend work

---
+++ index.js
@@ -0,0 +1,152 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const jwt = require("jsonwebtoken");
+require("dotenv").config()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json());
+const verifyJWT = (req, res, next) =>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({ error:true, message: 'Unauthorized access' })
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    if(err){
+      return res.status(402).send({error:true, message:"Unauthorized user access"})
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
+
+// WITH MONGODB CODE START
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xu7lgvl.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+
+    const reviewCollection = client.db('parlourDB').collection('reviews')
+    const usersCollection = client.db("parlourDB").collection("users");
+
+    app.post("/jwt", (req, res) =>{
+      const user = req.body;
+      const token =jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      })
+      res.send({token})
+    })
+    const verifyToken = ( req, res, next ) =>{
+      if(!req.headers.authorization){
+        return res.status(401).send({message:'unauthorized access'})
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message:'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next()
+      })
+    }
+    const verifyAdmin = async (req, res, next) =>{
+      const email = req.decoded.email;
+      const query = {email: email}
+      const user = await usersCollection.findOne(query)
+      const isAdmin = user?.role === "admin";
+      if(!isAdmin){
+        return res.status(403).send({message:'Forbidden access'})
+      }
+      next()
+    }
+    app.get('/users', verifyToken, verifyAdmin,  async(req, res) =>{
+      const result = await usersCollection.findOne().toArray()
+      res.send(result);
+    })
+
+    // user related apis
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await usersCollection.findOne(query);
+      if(existingUser){
+        return res.send({message:'User already Logged in'})
+      }
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+    /***
+     * firstlayer verifyJWT
+     * secondly email? == same
+     */
+    app.get('/users/admin/:email', verifyToken, async( req, res ) =>{
+      const email = req.params.email;
+      if(req.decoded.email !== email){
+        res.status(403).send({message:'forbidden access'})
+      }
+      const query = {email: email};
+      const user = await usersCollection.findOne(query)
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin'
+      }
+      res.send({ admin })
+    })
+
+    app.patch("/users/admin/:id", verifyToken, verifyAdmin, async (req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc ={
+        $set:{
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+    app.delete('/users/:id', verifyToken, verifyAdmin, async(req, res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.get('/review', async (req, res) =>{
+        const result = await reviewCollection.find().toArray();
+        res.send(result);
+    })
+
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// WITH MONGODB CODE END
+app.get('/', (req, res) =>{
+    res.send('Jeni apar parlour')
+})
+
+app.listen(port, () =>{
+    console.log(`Jenis parlorur running on port ${port}`);
+})
